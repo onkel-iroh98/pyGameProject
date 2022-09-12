d7b3fc9e75f61f17e8a66e1a5299127d56f54482
Nicklas <Nicklas.Rotsch@web.de>
2022-09-12 22:40:36 +0200

    zelda commit

--- tile.py
+++ tile.py
@@ -5,6 +5,6 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
 
         super().__init__(groups)
-        self.image = pygame.image.load("graphics/wall.png").convert_alpha()
+        self.image = pygame.image.load("graphics/test.png").convert_alpha()#arvorinha
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)  # mit -10 wird zB. auf beiden Seiten -5
