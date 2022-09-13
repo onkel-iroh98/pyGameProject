5c69209d7f88812eec3168e89975955f13105458
Nicklas <Nicklas.Rotsch@web.de>
2022-09-13 16:38:10 +0200

    MAP Doktoreien

--- settings.py
+++ settings.py
@@ -3,7 +3,8 @@ GAME_WIDTH = 720#1280
 GAME_HEIGHT = 480#720
 FPS = 60
 TILESIZE = 16 #hab es doch ziemlich runter geschaubt macht zu viele Probleme sonst (KampfScreen hat komplett andere Settings)
-
+#
+#
 
 
 
